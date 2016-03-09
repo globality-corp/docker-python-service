a7c38a947d4f122085ebb1eb976b987d40fe49a1
Jesse Myers <jesse.myers@globality.com>
2016-03-09 12:58:37 -0800

    Initial Dockerfile structure

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+# Container entrypoint to simplify running the production and dev servers.
+
+if [ "$1" = "uwsgi" ]; then
+    exec uwsgi --shared-socket :80 --http-socket =0 \
+               --uid nobody --gid nogroup \
+               --master --processes 10 \
+               --need-app --module ${NAME}.wsgi:app
+elif [ "$1" = "dev" ]; then
+    exec python manage.py runserver --host 0.0.0.0 --port 80
+else
+    exec $@
+fi
