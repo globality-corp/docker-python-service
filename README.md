40142779f89dc91dc18ae84615210db661580162
Jesse Myers <jesse.myers@globality.com>
2016-03-10 07:11:14 -0800

    Improvements to python image:  - Document build process  - Add a label  - Allow uWSGI num workers to be tuned via envvar

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # docker-python-service
-Base image for Python services
+
+Base image for Python services.
+
+
+## Building
+
+To build an untagged image locally:
+
+    docker build -t python-service .
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@
 if [ "$1" = "uwsgi" ]; then
     exec uwsgi --shared-socket :80 --http-socket =0 \
                --uid nobody --gid nogroup \
-               --master --processes 10 \
+               --master --processes ${UWSGI_NUM_PROCESSES:-4} \
                --need-app --module ${NAME}.wsgi:app
 elif [ "$1" = "dev" ]; then
     exec python manage.py runserver --host 0.0.0.0 --port 80
